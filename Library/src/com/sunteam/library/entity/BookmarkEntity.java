3568c0a939b8844abd42a7f0ef4a778579c1c763
wzp <wzp1029@126.com>
2017-02-13 21:33:54 +0800

    添加书签实体类。

---
+++ Library/src/com/sunteam/library/entity/BookmarkEntity.java
@@ -0,0 +1,27 @@
+package com.sunteam.library.entity;
+
+import java.io.Serializable;
+
+/**
+ * 书签实体类
+ * 
+ * @author wzp
+ * 
+ */
+public class BookmarkEntity implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8872374053931056239L;
+	
+    public int id;				//标识列
+    public String userName;		//用户名
+    public String bookId;		//书目ID
+    public String addedTime;	//创建时间
+    public int begin;			//字节流位置
+    public int chapterIndex;	//章节号
+    public String chapterTitle;	//章节名称
+    public String markName;		//书签名
+    public String percent;		//进度
+}
