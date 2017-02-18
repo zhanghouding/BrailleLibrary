34442945a122f3bb3f56ecab6bb01b998f79b792
zbc <zhubaochao08@163.com>
2017-02-18 15:16:59 +0800

    修改百分比格式化样式。

--- Library/src/com/sunteam/library/activity/PercentEdit.java
+++ Library/src/com/sunteam/library/activity/PercentEdit.java
@@ -43,7 +43,7 @@ public class PercentEdit extends BaseActivity {
 		if(0.0f == mPercentFloat){
 			mPercentStr = "0";
 		} else {
-			DecimalFormat decimalFormat = new DecimalFormat(".00");//构造方法的字符格式这里如果小数不足2位,会以0补足.
+			DecimalFormat decimalFormat = new DecimalFormat("0.00");//构造方法的字符格式这里如果小数不足2位,会以0补足.
 			mPercentStr = decimalFormat.format(mPercentFloat);
 		}
 
--- Library/src/com/sunteam/library/activity/PlayAudioVedioActivity.java
+++ Library/src/com/sunteam/library/activity/PlayAudioVedioActivity.java
@@ -401,7 +401,7 @@ public class PlayAudioVedioActivity extends Activity implements OnMediaPlayerLis
 		entity.chapterTitle = filename;
 		entity.markName = filename + " " + mTvStartTime.getText();
 		float percent = MediaPlayerUtils.getInstance().getPercent();
-		DecimalFormat decimalFormat = new DecimalFormat(".00");//构造方法的字符格式这里如果小数不足2位,会以0补足.
+		DecimalFormat decimalFormat = new DecimalFormat("0.00");//构造方法的字符格式这里如果小数不足2位,会以0补足.
 		entity.percent = decimalFormat.format(percent)+"%";
 		intent.putExtra("book_mark", entity);
 		intent.putExtra("percent", percent);
